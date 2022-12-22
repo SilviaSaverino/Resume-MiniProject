622420337ccb2e8e8849fb66d79dbfd54bba574a
Silvia Saverino <silviasave89@gmail.com>
2022-12-22 19:44:02 +0000

    add github file and initial js codes

---
+++ assets/js/github-information.js
@@ -0,0 +1,13 @@
+function fetchGitHubInformation(event) {
+    var username = $('#gh-username').val();
+    if (!username) {
+        $('#gh-user-data').html(`<h2>Please enter a GitHub username</h2>`)
+        return;
+    }
+}
+
+$.when(
+    $.getJSON('')
+)
+
+fetchGitHubInformation();
